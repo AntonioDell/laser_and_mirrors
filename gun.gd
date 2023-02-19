489d16a5698e5ea15e4541ad5718778cf334fea9
Antonio Dell'Annunziata <contact@antonio-da.dev>
2023-02-19 14:10:10 +0100

    feat: Remove mouse rotation

--- gun.gd
+++ gun.gd
@@ -3,14 +3,8 @@ extends Node2D
 
 @export var rotation_speed := 90.0
 
-var _mouse_pos: Vector2
-var _original_angle: float
-var _target_angle: float
-var _rotation_progress = 0.0
-
 
 func _physics_process(delta):
-	#_follow_mouse(delta)
 	_rotate_gun(delta)
 
 
@@ -24,14 +18,3 @@ func _rotate_gun(delta):
 	
 	var direction = Vector2.LEFT if rotate_counter_clockwise_pressed else Vector2.RIGHT 
 	rotation += direction.x * delta * deg_to_rad(rotation_speed)
-
-
-func _follow_mouse(delta):
-	if _mouse_pos != get_global_mouse_position():
-		_mouse_pos = get_global_mouse_position()
-		_original_angle = rotation
-		_target_angle = global_position.angle_to_point(_mouse_pos)
-		_rotation_progress = 0
-	if _rotation_progress < 1.0:
-		_rotation_progress += delta * rotation_speed
-		rotation = lerp_angle(_original_angle, _target_angle, ease(_rotation_progress, .5))
